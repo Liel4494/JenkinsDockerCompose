279d1828fcf149c6b99398c1aa42cf8250be4fb5
Liel4494 <43603550+Liel4494@users.noreply.github.com>
2024-12-30 23:46:43 +0200

    Update Curl_Build.groovy

--- Curl_Build.groovy
+++ Curl_Build.groovy
@@ -1,5 +1,5 @@
-stage("Clone Curl Repository"){
-    node("jenkins-master"){
+node("jenkins-master"){
+    stage("Clone Curl Repository"){
         cleanWs()
         
         checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/curl/curl.git']])   
@@ -8,10 +8,14 @@ stage("Clone Curl Repository"){
     }    
 }
 
-stage("Build Curl"){
-    node("jenkins-slave"){
+node("jenkins-slave"){
+    stage("Build Curl"){
         unstash 'curl-repo'
-        sh returnStdout: true, script: 'ls -l'
-        
+        sh returnStdout: true, script: 'autoreconf -fi'
+        sh returnStdout: true, script: './configure --with-openssl'
+        sh returnStdout: true, script: 'make'
+        sh returnStdout: true, script: 'make install'        
+        sh returnStdout: true, script: 'ls -la'
     }    
 }
+
